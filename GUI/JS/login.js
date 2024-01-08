87a39df74b34321f4aa356ff9c1f7d3711e051e6
Paulocesarhero <56269540+Paulocesarhero@users.noreply.github.com>
2024-01-07 20:38:24 -0600

    Paulo (#15)
    * agregue la interfaz grafica del login y modifique el dockerfile para eliminar las gui de la api rest
    
    * Agregue a un conductor a la bd desde el cliente con axios y puse la base url en main.js
    
    * Implementacion de nginx
    
    * modifique el login.html -> index.html para que sea la primera pagina que aparezca en el sistema
    
    * dependencias para archvios mime
    
    * Rapido arreglo para los ID
    
    * Subiendo rama actual
    
    * Subiendo rama actual
    
    * Agregue correcciones al contenedor de nginx
    
    * Agregue un poco de documentacion en swager para mayor legibilidad
    
    * puse comentarios y encripte los passwords
    
    * login con barrer token y registro de condctor listo
    
    * login
    
    * Ejemplo de login con redireccion y guardando el token en la gui con local storage
    
    ---------
    
    Co-authored-by: UlisesRamx <ulisesram19@gmail.com>

---
+++ GUI/JS/login.js
@@ -0,0 +1,81 @@
+function submitForm() {
+    const loginUrl = `${baseURL}/token`;
+    const protectedUrl = `${baseURL}/protected`;
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const postData = new URLSearchParams();
+    postData.append('grant_type', '');
+    postData.append('username', username);
+    postData.append('password', password);
+    postData.append('scope', '');
+    postData.append('client_id', '');
+    postData.append('client_secret', '');
+
+    // Paso 1: Obtener el token
+    axios.post(loginUrl, postData, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'accept': 'application/json'
+        }
+    })
+        .then(function (response) {
+            console.log('Respuesta del servidor (Login):', response);
+
+            // Verificar el contenido de la respuesta y actuar en consecuencia
+            if (response.data.access_token) {
+                const accessToken = response.data.access_token;
+
+                // Guardar el token en localStorage
+                localStorage.setItem('accessToken', accessToken);
+
+                // Paso 2: Obtener datos protegidos
+                axios.get(protectedUrl, {
+                    headers: {
+                        'accept': 'application/json',
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                })
+                    .then(function (protectedResponse) {
+                        console.log('Respuesta del servidor (Protected):', protectedResponse);
+
+                        // Verificar y almacenar el tipo de usuario en la GUI
+                        if (protectedResponse.data.current_user && protectedResponse.data.current_user.tipo) {
+                            const userType = protectedResponse.data.current_user.tipo;
+                            localStorage.setItem('userType', userType);
+                            if (userType == 'conductor'){
+                                window.location.href = 'html/MainUser.html';
+                            }if (userType == 'empleado'){
+                                window.location.href = 'html/MainAdmin.html';
+                            }
+                        }
+
+                        // Redirigir a la página principal del usuario
+                    })
+                    .catch(function (protectedError) {
+                        console.error('Error al obtener datos protegidos:', protectedError);
+                        // Manejar el error al obtener datos protegidos
+                    });
+            } else {
+                // La respuesta no contiene el token esperado
+                alert('Respuesta del servidor inesperada');
+            }
+        })
+        .catch(function (error) {
+            console.error('Error al realizar la solicitud (Login):', error);
+
+            // Manejar el error como desees
+            if (error.response) {
+                // El servidor respondió con un código de estado diferente de 2xx
+                console.log('Código de estado:', error.response.status);
+                console.log('Mensaje del servidor:', error.response.data);
+            } else if (error.request) {
+                // La solicitud fue hecha pero no se recibió una respuesta
+                console.log('No se recibió respuesta del servidor');
+            } else {
+                // Algo sucedió en la configuración de la solicitud que desencadenó el error
+                console.log('Error de configuración de la solicitud:', error.message);
+            }
+        });
+}
--- security/security.py
+++ security/security.py
@@ -1,8 +1,52 @@
+from datetime import datetime, timedelta
+
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
+from decouple import config
+from starlette import status
+
+JWT_SECRET = config("SECRET_KEY")
+JWT_ALGORITHM = config("ALGORITHM")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def verificarPassword(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="No se pudo validar las credenciales",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        email: str = payload.get("email")
+        tipo: str = payload.get("tipo")
+        id: str = payload.get("id")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    return {"username": email,"tipo": tipo, "id": id, "token": token}
